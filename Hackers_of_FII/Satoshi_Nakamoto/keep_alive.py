5512a8d85be7c9a262dc42d9d3083a5d5ec839ce
reloadedd <reloadedd@protonmail.com>
2022-10-08 18:00:38 +0300

    Use waitress for the keep alive script

--- Hackers_of_FII/Satoshi_Nakamoto/keep_alive.py
+++ Hackers_of_FII/Satoshi_Nakamoto/keep_alive.py
@@ -2,6 +2,7 @@
 
 from flask import Flask
 from threading import Thread
+from waitress import serve
 
 app = Flask('')
 
@@ -12,9 +13,9 @@ def home():
 
 
 def run():
-    app.run(host='0.0.0.0', port=8080)
+    serve(app, host='0.0.0.0', port=8080)
 
 
 def keep_alive():
-  t = Thread(target=run)
-  t.start()
+    t = Thread(target=run)
+    t.start()
